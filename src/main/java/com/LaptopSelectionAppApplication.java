4a561c0c8661c95bf81235224c5ed462f7d49f01
grits <grits.0123@gmail.com>
2020-05-14 13:43:33 +0300

    The second time commiting Application class

---
+++ src/main/java/com/LaptopSelectionAppApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopSelectionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopSelectionAppApplication.class, args);
+	}
+
+}
